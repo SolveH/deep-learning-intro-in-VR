91662657dc0362394cdaf391230fa5b87016b016
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2019-12-02 18:40:31 +0100

    Add coffee interact logic and objectivemanager

---
+++ Assets/Scripts/ObjectiveHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ObjectiveHandler : MonoBehaviour
+{
+    public GameObject coffeeSnapZone;
+    public TMP_Text text;
+    public GameObject interactableCoffecup;
+
+    void Update()
+    {
+        if(interactableCoffecup.transform.position.y < 0.2)
+        {
+            interactableCoffecup.transform.position = new Vector3(2.023f, 0.943f, -2.896f);
+        }
+    }
+
+    public void firstGrabbedCoffee()
+    {
+        coffeeSnapZone.SetActive(true);
+        text.text = "Put the coffee cup in the coffee machine";
+        
+    }
+}
