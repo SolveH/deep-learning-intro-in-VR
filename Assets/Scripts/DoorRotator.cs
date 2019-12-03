13897947b28553db89886b24374cc0817e908065
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2019-12-03 16:59:26 +0100

    Add door rotation and table

---
+++ Assets/Scripts/DoorRotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorRotator : MonoBehaviour
+{
+    private float rotationSpeed = 0.01f;
+    private float y = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(y > -150)
+        {
+            y += Time.deltaTime * -20;
+            transform.rotation = Quaternion.Euler(0, y, 0);
+        }
+    }
+}
