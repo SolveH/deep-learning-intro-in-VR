087dde13b6e5cde5502df3daff6bb2a0fe3e13ce
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2019-12-04 18:44:37 +0100

    Finish v1

--- Assets/Scripts/ObjectiveHandler.cs
+++ Assets/Scripts/ObjectiveHandler.cs
@@ -8,14 +8,23 @@ public class ObjectiveHandler : MonoBehaviour
 {
     //General objects
     public GameObject roof;
+    public GameObject headsetAlias;
 
     //Coffee objects
+    private bool task1finished = false;
+    private bool task2finished = false;
     public GameObject coffeeSnapZone;
     public TMP_Text tv_text;
 
     //Teleport objects
+    private bool task3finished = false;
+    public GameObject teleportTV;
     public GameObject teleportTarget;
     public TMP_Text teleport_text;
+
+    //Key objects
+    public GameObject keygrabbingTVCanvas;
+    public GameObject key;
 
     void Start()
     {
@@ -24,25 +33,39 @@ public class ObjectiveHandler : MonoBehaviour
 
     void Update()
     {
-        
+        //Disable teleport target and teleport tutorial TV when player is near teleport target 1
+        //Also enable keygrab TV tutorial
+        if (teleportTarget.activeSelf && !task3finished)
+        {
+            if (Vector3.Distance(headsetAlias.transform.position, teleportTarget.transform.position) < 3)
+            {
+                task3finished = true;
+                teleport_text.text = "You can teleport while holding an item";
+                keygrabbingTVCanvas.SetActive(true);
+            }
+        }
     }
 
     public void firstGrabbedCoffee()
     {
+        task1finished = true;
         coffeeSnapZone.SetActive(true);
         tv_text.text = "Put the coffee cup in the coffee machine";
-        
     }
 
     public void coffeeSnapped()
     {
+        task2finished = true;
         tv_text.text = "Turn left";
         teleportTarget.SetActive(true);
-        teleport_text.text = "Hold your thumb on the joystick and point at the teleport target";
+        teleport_text.text = "Pull the joystick forward and point at the teleport target";
     }
 
     public void teleportTargetEntered()
     {
-        teleport_text.text = "Press the joystick down while pointing at teleport target";
+        if(!task3finished)
+        {
+            teleport_text.text = "Release the joystick to teleport";
+        }
     }
 }
