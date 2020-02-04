f6980d1ef95ea05b57a178c581efda0cf41b750f
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2020-02-04 15:27:11 +0100

    Create perception room with half-way implemented task

---
+++ Assets/Scripts/Perceptron.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perceptron : MonoBehaviour
+{
+    [SerializeField]
+    private int bias = -3;
+    [SerializeField]
+    private int w1 = -2;
+    [SerializeField]
+    private int w2 = -2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
