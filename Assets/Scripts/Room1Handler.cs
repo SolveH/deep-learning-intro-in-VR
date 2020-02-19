3229602899d4ca18ef84fde4dce165b6e10815b3
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2020-02-19 12:09:53 +0100

    Add quiz screen and change room 3 layout

---
+++ Assets/Scripts/Room1Handler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room1Handler : MonoBehaviour
+{
+    public GameObject ceiling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ceiling.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
