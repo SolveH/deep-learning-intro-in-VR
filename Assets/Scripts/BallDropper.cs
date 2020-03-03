15d7fff52847516e4aa7d130253ba82fb8bade84
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2020-03-03 15:37:06 +0100

    Add trajectory to gradient descent and learning materials

--- Assets/Scripts/BallDropper.cs
+++ Assets/Scripts/BallDropper.cs
@@ -8,9 +8,11 @@ using System;
 public class BallDropper : MonoBehaviour
 {
     public GameObject ballPrefab;
+    public GameObject trajectoryBall;
     public TMP_Text gradientDescentValuesText;
 
     private List<Vector3> startPositions;
+    private List<GameObject> trajectoryBalls;
 
     private int currentBall = 0;
 
@@ -18,11 +20,15 @@ public class BallDropper : MonoBehaviour
     void Start()
     {
         startPositions = new List<Vector3>();
+        trajectoryBalls = new List<GameObject>();
+
         startPositions.Add(new Vector3(-0.569f, 2.618f, 2.419f));
         startPositions.Add(new Vector3(1.977f, 3.098f, 2.333f));
         startPositions.Add(new Vector3(-1.023f, 2.922f, -2.026f));
         startPositions.Add(new Vector3(1.414f, 2.842f, -2.198f));
         startPositions.Add(new Vector3(0.036f, 2.442f, -0.71f));
+
+        InvokeRepeating("SpawnTrajectoryBall", 0f, 0.15f);
     }
 
     // Update is called once per frame
@@ -54,8 +60,26 @@ public class BallDropper : MonoBehaviour
             if (ball.GetComponent<Rigidbody>().velocity.magnitude < 0.01f)
             {
                 Destroy(ball);
+                foreach(GameObject trajBall in trajectoryBalls)
+                {
+                    Destroy(trajBall);
+                }
+                trajectoryBalls.Clear();
             }
         }
+    }
+    private void SpawnTrajectoryBall()
+    {
+        GameObject ball = GameObject.Find("Gradient ball(Clone)");
+        if(ball != null)
+        {
+            GameObject newTrajectoryBall = Instantiate(trajectoryBall) as GameObject;
+            newTrajectoryBall.transform.SetParent(this.transform);
+            Vector3 ballLocalPosition = ball.transform.localPosition;
+            newTrajectoryBall.transform.localPosition = new Vector3(ballLocalPosition.x, ballLocalPosition.y - 0.15f, ballLocalPosition.z);
+            trajectoryBalls.Add(newTrajectoryBall);
+        }
         
+
     }
 }
