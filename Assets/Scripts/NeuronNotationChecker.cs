33da998af0545a6f8ae2bfc3f185ea1fa3e85fdf
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2020-03-12 11:08:35 +0100

    Add materials and start develop notation task

---
+++ Assets/Scripts/NeuronNotationChecker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuronNotationChecker : MonoBehaviour
+{
+    public int layer;
+    public int neuronIndex;
+
+    private AudioSource incorrectAudio;
+    private AudioSource correctAudio;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        incorrectAudio = audioSources[0];
+        correctAudio = audioSources[1];
+        
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void insertNeuron(GameObject neuron)
+    {
+        NeuronNotationNeuron neuronScript = neuron.GetComponent<NeuronNotationNeuron>();
+        if(neuronScript.getNeuronLayer() != layer || neuronScript.getNeuronIndex() != neuronIndex)
+        {
+            incorrectAudio.Play();
+        }
+        else
+        {
+            correctAudio.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/NeuronNotationNeuron.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NeuronNotationNeuron : MonoBehaviour
+{
+    public int neuronLayer;
+    public int neuronIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP_Text indicesText;
+        foreach (TMP_Text text in GetComponentsInChildren<TMP_Text>())
+        {
+            if(text.name == "Indices")
+            {
+                indicesText = text;
+                indicesText.text = "" + neuronLayer + "\n" + neuronIndex;
+                break;
+            }
+        }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public int getNeuronLayer()
+    {
+        return neuronLayer;
+    }
+    public int getNeuronIndex()
+    {
+        return neuronIndex;
+    }
+}
