0d8474dadb451e11d1dd2415745b9949cd1be63a
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2020-04-01 15:32:41 +0200

    Add credits

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,9 +7,17 @@ public class UIManager : MonoBehaviour
 {
     public Button tutorialBtn;
     public Button room1Btn;
+    public GameObject leftScreen;
+    public GameObject midScreen;
+    public GameObject rightScreen;
+
+    public GameObject credits;
+    private Animator creditsAnimator;
 
     private void Awake()
     {
+        creditsAnimator = credits.GetComponent<Animator>();
+        
         tutorialBtn.onClick.AddListener(() =>
         {
             SceneLoader.Load(SceneLoader.Scene.Tutorial);
@@ -28,6 +36,27 @@ public class UIManager : MonoBehaviour
 
     public void OpenSurveyInBrowser()
     {
-        Application.OpenURL("https://forms.gle/Q6vwbRYoscrng9er9");
+        Application.OpenURL("https://forms.gle/kxvfuiEaJCGM7bbE6");
+    }
+
+    public void PlaySubtitles()
+    {
+        creditsAnimator.SetTrigger("PlayCredits");
+        leftScreen.SetActive(false);
+        midScreen.SetActive(false);
+        rightScreen.SetActive(false);
+        StartCoroutine(ActivateScreensAfterTime(13.0f));
+
+    }
+
+    private IEnumerator ActivateScreensAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        creditsAnimator.ResetTrigger("PlayCredits");
+        leftScreen.SetActive(true);
+        midScreen.SetActive(true);
+        rightScreen.SetActive(true);
     }
 }
